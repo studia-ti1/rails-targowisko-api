ce245587ca6e14f6eefefc9f3adf2ef3ca17948a
Tomasz Kot <kottomasz98@gmail.com>
2019-10-13 13:57:58 +0200

    Add session permitted params

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -5,9 +5,9 @@ module Api
     class SessionsController < ApplicationController
       # TODO: permitted params
       def create
-        user = User.find_by(email: params[:user][:email])
+        user = User.find_by(email: session_params[:email])
 
-        if user&.valid_password?(params[:user][:password])
+        if user&.valid_password?(session_params[:password])
           @current_user = user
         else
           render json: { errors: { 'email or password' => ['is invalid'] } }, status: :unprocessable_entity
@@ -20,6 +20,12 @@ module Api
         jwt_payload = JwtHelper.decode(token: request.headers['Authorization'].split(' ').second)
         jti = jwt_payload['jti']
         JwtBlacklist.create!(jti: jti)
+      end
+
+      private
+
+      def session_params
+        params.require(:user).permit(:email, :password)
       end
     end
   end
